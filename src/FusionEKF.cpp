d294e8e35155df2aad4692e6a28402b456c2a7db
robert preissl <robert@roberts-MacBook.local>
2017-03-10 08:21:56 -0800

    wip

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -26,6 +26,12 @@ FusionEKF::FusionEKF() {
   TODO:
     * Finish initializing the FusionEKF.
   */
+  R_radar_ << 0.0225, 0, 0,
+              0, 0.0225, 0,
+              0, 0, 0.0225;
+
+  R_laser_ << 0.0225, 0,
+              0, 0.0225;
 
   //set the acceleration noise components
   noise_ax = 5;
@@ -126,6 +132,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
    */
 
+   //compute the time elapsed between the current and previous measurements
+   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+   cout << dt << endl;
+   previous_timestamp_ = measurement_pack.timestamp_;
+
+   float dt_2 = dt * dt;
+   float dt_3 = dt_2 * dt;
+   float dt_4 = dt_3 * dt;
+
+   //Modify the F matrix so that the time is integrated
+   ekf_.F_(0, 2) = dt;
+   ekf_.F_(1, 3) = dt;
+
+   //set the process covariance matrix Q
+   ekf_.Q_ = MatrixXd(4, 4);
+   ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+          0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+          dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+          0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+
   ekf_.Predict();
 
   /*****************************************************************************
@@ -140,8 +166,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.Update(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   }
 
   // print the output
